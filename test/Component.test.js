285a804f3d253a618afa1074c27e8d026c8cefab
Polifev <pol007@skynet.be>
2020-08-31 16:58:35 +0200

    Setup coverage for unit tests

---
+++ test/Component.test.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+const { Component } = require("../src/Component");
+
+// TODO
---
+++ test/Gallimimus.test.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+const { Gallimimus } = require("../src/Gallimimus");
+
+// TODO
